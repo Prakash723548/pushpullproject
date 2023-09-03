29132e1acfe4d0194ee24d2465a8d612dc908e7f
Prakash723548 <asampoorprakash@gmail.com>
2023-09-03 13:04:31 +0530

    initial commit

---
+++ src/com/fusion/Demo1.java
@@ -0,0 +1,10 @@
+package com.fusion;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("Prakash asampoor");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author PRAKASH
+ *
+ */
+module Project1 {
+}
